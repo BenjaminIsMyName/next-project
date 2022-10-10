4570f0a5a8cd41039f520a1e5cd26869a2eef05b
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2022-10-10 20:29:15 +0300

    Added custom font + bug solving
    1. Added custom font from Google Fonts.
    2. Better animation for menu button.
    3. Fixed bug for buttons, when red line was overflowing in some cases.

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html dir='rtl'>
-      <Head />
+      <Head>
+        {/* adding font, see: https://nextjs.org/docs/basic-features/font-optimization */}
+        {/* I'm using "&display=swap" because I want it to swap the font after it was loaded,
+            see: https://nextjs.org/docs/messages/google-font-display
+            if I won't do it, the font will not be shown on initial load....  */}
+        <link
+          href='https://fonts.googleapis.com/css2?family=Rubik+Dirt&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
       {/* When sending the documant to the client, send it with RTL.
           That way, most users (RTL users) won't see a flicker.
           TODO: try to find a way to change it on the server already, based on user's language.
