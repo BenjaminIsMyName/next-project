6f007e4a31111c19f39bac87f7d0b317c7c79b0b
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2022-10-29 21:23:09 +0300

    Filter and then fetch
    Instead of fetching all from DB and filtering them on the server.

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,6 +1,7 @@
 import { getCookie } from "cookies-next";
 import { isLoggedInFunc } from "../../util/authHelpers";
 import connectToDatabase from "../../util/mongodb";
+import { ObjectId } from "mongodb";
 
 export default async function handler(req, res) {
   if (req.method !== "GET") return;
@@ -20,7 +21,10 @@ export default async function handler(req, res) {
   // Fetch posts:
   try {
     const { db } = await connectToDatabase();
-    var posts = await db.collection("posts").find().toArray();
+    var posts = await db
+      .collection("posts")
+      .find({ _id: { $nin: EXIST.map(o => ObjectId(o)) } })
+      .toArray(); // TODO: try to sort and limit everything right here, instead of fetching all and doing sort & limit on the server
   } catch (err) {
     console.log(err);
     res.status(503).json({ error: `failed to get posts from DB: ${err}` });
@@ -42,26 +46,8 @@ export default async function handler(req, res) {
     else return 0;
   });
 
-  // Remove posts that are already on the client side:
-  // ------ short version:
-  const filteredPosts = posts.filter(
-    p => (EXIST.find(i => p._id.equals(i)) ? false : true) // see about 'equals': https://futurestud.io/tutorials/mongodb-how-to-compare-objectids-in-node-js
-  );
-  // ------ long version:
-  // const filteredPosts = [];
-  // for (let i = 0; i < posts.length; i++) {
-  //   let isExist = false;
-  //   for (let j = 0; j < EXIST.length; j++) {
-  //     if (JSON.stringify(posts[i]._id) === JSON.stringify(EXIST[j])) {
-  //       isExist = true;
-  //       break;
-  //     }
-  //   }
-  //   if (!isExist) filteredPosts.push(posts[i]);
-  // }
-
   // Send only some of the posts, the amount that was requested
-  const getSome = filteredPosts.slice(0, AMOUNT);
+  const getSome = posts.slice(0, AMOUNT);
 
   // Remove unnecessary data, show "didLike" if user is logged in etc
   const customize = getSome.map(p => {
@@ -82,9 +68,7 @@ export default async function handler(req, res) {
         : false,
     };
   });
-  res
-    .status(200)
-    .send({ posts: customize, hasMore: filteredPosts.length > AMOUNT });
+  res.status(200).send({ posts: customize, hasMore: posts.length > AMOUNT });
 }
 
 function getNumberOfMinutesPassed(date) {
