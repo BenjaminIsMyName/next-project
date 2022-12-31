2d98c2ccdf2a38961e998806502240146933bde5
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2023-01-01 00:37:02 +0200

    A little bit of animation

--- components/admin/SelectedTopic.js
+++ components/admin/SelectedTopic.js
@@ -1,8 +1,13 @@
+import { motion } from "framer-motion";
+
 export default function SelectedTopic({ text, removeCallback }) {
   return (
-    <div className="w-max inline-flex gap-4 items-center justify-center bg-main-color py-2 px-5 w-full rounded-lg [&_svg]:w-4 [&_svg]:fill-option-text-color shadow-md shadow-shadows-color">
+    <motion.div
+      layout
+      className="w-max inline-flex gap-4 items-center justify-center bg-main-color py-2 px-5 rounded-lg [&_svg]:w-4 [&_svg]:fill-option-text-color shadow-md shadow-shadows-color"
+    >
       <span className="whitespace-nowrap">{text}</span>
       <button onClick={removeCallback}>⛔</button>
-    </div>
+    </motion.div>
   );
 }
