673c83e834e40fa3fa255a7c8cca44846fa9f312
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2023-03-16 21:08:56 +0200

    Fixed the environment variable I renamed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://redilet.com",
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://redilet.com",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
