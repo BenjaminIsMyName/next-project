08f93981e62b5a0cb603f67750b5154ee205452e
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2023-03-20 15:27:17 +0200

    More framer motion in progress bar

--- components/Video.js
+++ components/Video.js
@@ -1,7 +1,7 @@
 import { useRef, useState } from "react";
 import ReactPlayer from "react-player";
 import LikeIcon from "./icons/LikeIcon";
-import { motion as m } from "framer-motion";
+import { AnimatePresence, motion as m } from "framer-motion";
 import { useRouter } from "next/router";
 
 export default function CustomVideoPlayer({ videoUrl, setCanPlay }) {
@@ -36,86 +36,101 @@ export default function CustomVideoPlayer({ videoUrl, setCanPlay }) {
         className="absolute inset-0 z-10 cursor-pointer"
         onClick={() => setIsPlaying(prev => !prev)}
       ></div>
-      {(isHovering || !isPlaying) && (
-        <div className="absolute bottom-0 left-0 right-0 h-20 bg-gradient-to-t from-option-text-color/70 via-option-text-color/50 to-main-color/0 z-20 p-4 pt-3">
-          {/* play/pause button, volume, times (numbers and scrollbar), full screen, pip  */}
+      <AnimatePresence>
+        {(isHovering || !isPlaying) && (
+          <m.div
+            initial={{ opacity: 0, scaleY: 0, y: 100 }}
+            animate={{
+              opacity: 1,
+              scaleY: 1,
+              y: 0,
+              transition: { duration: 0.2, ease: "easeOut" },
+            }}
+            exit={{
+              opacity: 0,
+              transition: { duration: 1, ease: "easeIn", delay: 1 },
+            }}
+            className="absolute bottom-0 left-0 right-0 h-20 bg-gradient-to-t from-option-text-color/70 via-option-text-color/50 to-main-color/0 z-20 p-4 pt-3"
+          >
+            {/* play/pause button, volume, times (numbers and scrollbar), full screen, pip  */}
 
-          <div>
-            {/* container for all the top part */}
-            <div className="flex gap-4">
-              {/* container for the left part */}
-              {/* play icon: */}
-              <button
-                onClick={() => setIsPlaying(prev => !prev)}
-                className="backdrop-blur-xl p-2 rounded-lg"
-              >
-                {isPlaying ? (
-                  <svg className="w-6 h-6">
-                    <path d="M0 0h24v24H0z" fill="none" />
-                    <path
-                      d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"
-                      className="fill-second-color"
-                    />
-                  </svg>
-                ) : (
-                  <svg className="w-6 h-6">
-                    <path d="M0 0h24v24H0z" fill="none" />
-                    <path d="M8 5v14l11-7z" className="fill-second-color" />
-                  </svg>
-                )}
-              </button>
-              {/* time */}
-              <div className="flex items-center gap-2 backdrop-blur-xl p-2 rounded-lg">
-                <span className="text-second-color text-opacity-70">
-                  {formatTime(playedSeconds)}
-                </span>
-                <span className="text-second-color text-opacity-70">/</span>
-                <span className="text-second-color text-opacity-100">
-                  {formatTime(duration)}
-                </span>
+            <div>
+              {/* container for all the top part */}
+              <div className="flex gap-4">
+                {/* container for the left part */}
+                {/* play icon: */}
+                <button
+                  onClick={() => setIsPlaying(prev => !prev)}
+                  className="backdrop-blur-xl p-2 rounded-lg"
+                >
+                  {isPlaying ? (
+                    <svg className="w-6 h-6">
+                      <path d="M0 0h24v24H0z" fill="none" />
+                      <path
+                        d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"
+                        className="fill-second-color"
+                      />
+                    </svg>
+                  ) : (
+                    <svg className="w-6 h-6">
+                      <path d="M0 0h24v24H0z" fill="none" />
+                      <path d="M8 5v14l11-7z" className="fill-second-color" />
+                    </svg>
+                  )}
+                </button>
+                {/* time */}
+                <div className="flex items-center gap-2 backdrop-blur-xl p-2 rounded-lg">
+                  <span className="text-second-color text-opacity-70">
+                    {formatTime(playedSeconds)}
+                  </span>
+                  <span className="text-second-color text-opacity-70">/</span>
+                  <span className="text-second-color text-opacity-100">
+                    {formatTime(duration)}
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-          {/* container for the bottom part */}
-          <div
-            ref={progressBarRef}
-            onClick={e => {
-              // get position of the click in the progress bar
-              const rect = progressBarRef.current.getBoundingClientRect();
-              const x =
-                locale === "en"
-                  ? e.clientX - rect.left
-                  : rect.right - e.clientX;
-              // calculate the percentage of the click
-              const percentage = x / rect.width;
-              // calculate the time of the video
-              const time = Math.floor(percentage * duration); // Math.floor() is needed because seekTo() doesn't accept decimals (weird behavior when passing 0.someNumber)
-              // set the time of the video
-              setPlayedSeconds(time); // much faster than waiting for seekTo() to finish
-              // jump to the time
-              playerRef.current.seekTo(time);
-            }}
-            className="cursor-pointer group w-full backdrop-blur-xl rounded-lg h-2 mt-2 bg-main-color/20 relative hover:scale-y-150 transition-all"
-          >
-            <m.div
-              layout
-              style={{
-                width: currentWidthOfProgressBar + "%",
-              }}
-              className={`absolute inset-0 bg-gradient-to-tl from-main-color to-third-color rounded-lg`}
-            >
-              {/* thumb indicator (round little thing in the progress bar) */}
-              <div
-                className={`rounded-full h-4 w-4 bg-third-color absolute ${
+            {/* container for the bottom part */}
+            <div
+              ref={progressBarRef}
+              onClick={e => {
+                // get position of the click in the progress bar
+                const rect = progressBarRef.current.getBoundingClientRect();
+                const x =
                   locale === "en"
-                    ? "right-0 translate-x-1/2"
-                    : "left-0 -translate-x-1/2"
-                } top-1/2 -translate-y-1/2 scale-0 group-hover:scale-x-100 group-hover:scale-y-75 transition-transform`}
-              ></div>
-            </m.div>
-          </div>
-        </div>
-      )}
+                    ? e.clientX - rect.left
+                    : rect.right - e.clientX;
+                // calculate the percentage of the click
+                const percentage = x / rect.width;
+                // calculate the time of the video
+                const time = Math.floor(percentage * duration); // Math.floor() is needed because seekTo() doesn't accept decimals (weird behavior when passing 0.someNumber)
+                // set the time of the video
+                setPlayedSeconds(time); // much faster than waiting for seekTo() to finish
+                // jump to the time
+                playerRef.current.seekTo(time);
+              }}
+              className="cursor-pointer group w-full backdrop-blur-xl rounded-lg h-2 mt-2 bg-main-color/20 relative hover:scale-y-150 transition-all"
+            >
+              <m.div
+                layout
+                style={{
+                  width: currentWidthOfProgressBar + "%",
+                }}
+                className={`absolute inset-0 bg-gradient-to-tl from-main-color to-third-color rounded-lg`}
+              >
+                {/* thumb indicator (round little thing in the progress bar) */}
+                <div
+                  className={`rounded-full h-4 w-4 bg-third-color absolute ${
+                    locale === "en"
+                      ? "right-0 translate-x-1/2"
+                      : "left-0 -translate-x-1/2"
+                  } top-1/2 -translate-y-1/2 scale-0 group-hover:scale-x-100 group-hover:scale-y-75 transition-transform`}
+                ></div>
+              </m.div>
+            </div>
+          </m.div>
+        )}
+      </AnimatePresence>
       <ReactPlayer
         ref={playerRef}
         onEnded={() => setIsPlaying(false)}
