0e06f187ace8cc5cd79a21ec4f9d2ca10209a5fa
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2023-04-05 01:00:37 +0300

    Fixing status bar color when installed as an app

--- pages/_document.js
+++ pages/_document.js
@@ -59,9 +59,9 @@ export default function Document() {
         href="/favicon-16x16.png"
       />
       <link rel="manifest" href="/manifest.json" />
-      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="msapplication-TileColor" content="#232323" />
       <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-      <meta name="theme-color" content="#ffffff"></meta>
+      <meta name="theme-color" content="#232323"></meta>
       {/* ----- All the code & files favicon-generator.org generated ----- */}
       <Head>
         {/* adding font, see: https://nextjs.org/docs/basic-features/font-optimization */}
