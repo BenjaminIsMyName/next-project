2ff30b4310e563fe87bfb6908d564abddc35e0d0
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2023-04-17 02:02:03 +0300

    Slower transition, more obvious

--- components/admin/Container.js
+++ components/admin/Container.js
@@ -15,7 +15,8 @@ export default function Container({
         initial={shouldAnimateIn ? { scale: 0, rotate: 0, opacity: 0 } : {}}
         animate={{ scale: 1, rotate: 0, opacity: 1 }}
         exit={shouldAnimateOut ? { scale: 0, rotate: 0, opacity: 0 } : {}}
-        transition={{ ease: "easeIn", duration: 0.5 }}
+        // cubic-bezier(.17,.67,.14,.9) - fast at the beginning, slow at the end
+        transition={{ ease: [0.17, 0.67, 0.14, 0.9], duration: 1 }}
         className={`absolute top-0 left-0 right-0 min-h-[1000px] text-option-text-color 
         bg-second-color/80 z-20 backdrop-blur-md border-b-[20px] border-main-color
         pb-[var(--header-height)] md:pb-0 p-[min(20px,3%)] md:p-5 md:border-[20px] border-0 
